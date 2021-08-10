08b8857f791d98886a09772e7981760cf412ee39
Chiranjeeb Nayak <chiranjeebnayak.37@gmail.com>
2021-08-11 01:22:18 +0530

    experience update

--- src/components/timelineElements.js
+++ src/components/timelineElements.js
@@ -1,5 +1,15 @@
 let timelineElements = [
   {
+    id: 2,
+    title: "Software Developer ",
+    location: "SinoxFX,Lucknow",
+    description:
+      "Working hand-in-hand with front-end developers by providing the outward facing web application elements server-side logic. Creating the logic to make the web app function properly, and accomplishing this through the use of server-side scripting languages.",
+    buttonText: "View Backend Projects",
+    date: "June 2013 - August 2016",
+    icon: "work",
+  },
+  {
     id: 1,
     title: "Frontend Developer",
     location: "Shopsky PVT LTD,Jeypore",
@@ -7,16 +17,6 @@ let timelineElements = [
       "Converting data to a graphical interface, through the use of HTML, CSS, and JavaScript, so that users can view and interact with that data.",
     buttonText: "Certificate",
     date: "August 2016 - present",
-    icon: "work",
-  },
-  {
-    id: 2,
-    title: "Software Developer ",
-    location: "Sinoxfx,Lucknow",
-    description:
-      "Working hand-in-hand with front-end developers by providing the outward facing web application elements server-side logic. Creating the logic to make the web app function properly, and accomplishing this through the use of server-side scripting languages.",
-    buttonText: "View Backend Projects",
-    date: "June 2013 - August 2016",
     icon: "work",
   },
   {
